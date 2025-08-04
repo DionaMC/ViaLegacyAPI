17a1919c88e8b6c6e68c116e24f05e22c53ec218
Loyisa <30242618+Loyisa@users.noreply.github.com>
2025-08-04 23:14:49 +0800

    1.0.3

--- src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
+++ src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
@@ -94,6 +94,8 @@ public class ProtocolVersion {
     public static final ProtocolVersion v1_21_2 = register(768, "1.21.2/1.21.3", new SubVersionRange("1.21", 2, 3));
     public static final ProtocolVersion v1_21_4 = register(769, "1.21.4");
     public static final ProtocolVersion v1_21_5 = register(770, "1.21.5");
+    public static final ProtocolVersion v1_21_6 = register(771, "1.21.6");
+    public static final ProtocolVersion v1_21_7 = register(772, "1.21.7-1.21.8", new SubVersionRange("1.21", 7, 8));
     public static final ProtocolVersion unknown = register(-1, "UNKNOWN");
 
     public static ProtocolVersion register(int version, String name) {
